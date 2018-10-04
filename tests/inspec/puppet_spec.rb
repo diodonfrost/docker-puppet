2142384c232a4ea27b5f3637d3e9e737e3fc1cb1
diodonfrost <diodon.frost@diodonfrost.me>
2018-10-04 15:03:19 +0200

    add inspec support for gentoo image

--- tests/inspec/puppet_spec.rb
+++ tests/inspec/puppet_spec.rb
@@ -3,12 +3,17 @@
 puppet_package = "puppet-agent"
 puppet_package = "puppet" if os.name == "arch" or os.name == "gentoo"
 
-
 control 'puppet-01' do
   impact 1.0
   title 'Puppet agent install'
   desc 'Puppet agent should be installed'
-  describe package(puppet_package) do
-    it { should be_installed }
+  if os.name != "gentoo"
+    describe package(puppet_package) do
+      it { should be_installed }
+    end
+  elsif os.name == "gentoo"
+    describe command('eix -I puppet') do
+      its('exit_status') { should eq 0 }
+    end
   end
 end
