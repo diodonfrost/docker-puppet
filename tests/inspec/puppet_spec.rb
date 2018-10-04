74e7942f13d0657c6cdae99cbf3a03cbf53fc341
diodonfrost <diodon.frost@diodonfrost.me>
2018-10-04 17:10:25 +0200

    remove gentoo tests, building image too long for travis

--- tests/inspec/puppet_spec.rb
+++ tests/inspec/puppet_spec.rb
@@ -1,19 +1,13 @@
 # Test for puppet docker container
 
 puppet_package = "puppet-agent"
-puppet_package = "puppet" if os.name == "arch" or os.name == "gentoo"
+puppet_package = "puppet" if os.name == "arch"
 
 control 'puppet-01' do
   impact 1.0
   title 'Puppet agent install'
   desc 'Puppet agent should be installed'
-  if os.name != "gentoo"
-    describe package(puppet_package) do
-      it { should be_installed }
-    end
-  elsif os.name == "gentoo"
-    describe command('eix -I puppet') do
-      its('exit_status') { should eq 0 }
-    end
+  describe package(puppet_package) do
+    it { should be_installed }
   end
 end
